ea07f794a1704cc54c3c8b5352712091c6dccbc1
薛笔刀 <2985095572@qq.com>
2024-12-29 18:55:13 +0800

    feat：添加演示视频

--- main.cpp
+++ main.cpp
@@ -1,22 +1,6 @@
 #include "widget.h"
 #include <QApplication>
 
-
-// void *operator new(size_t size){
-//     void *ptr = malloc(size);
-//     if(ptr == nullptr){
-//         throw std::bad_alloc();
-//     }
-//     // printf("new: ptr = %p,size = %zd\n",ptr,size);
-//     qDebug()<<"new: ptr "<<ptr<<"size = "<<size;
-//     return ptr;
-// }
-
-// void operator delete(void *ptr) noexcept{
-//     // printf("delete :ptr = %p\n",ptr);
-//     qDebug()<<"delete : ptr = "<<ptr;
-//     free(ptr);
-// }
 int main(int argc, char *argv[])
 {
 
